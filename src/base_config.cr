24d4693ad121141fa6eb757a09f96cb47f4cd86f
Blacksmoke16 <yomoejoe@gmail.com>
2019-12-29 14:24:51 -0500

    Add method to load config

--- src/base_config.cr
+++ src/base_config.cr
@@ -1,8 +1,7 @@
-require "yaml"
-
 # Base config type that wraps the `Athena` configuration file.
 #
 # Components may add their own types to `self` to allow configuring that specific component.
 struct Athena::Config::Config
   include YAML::Serializable
+  include YAML::Serializable::Strict
 end
--- src/config.cr
+++ src/config.cr
@@ -1,3 +1,7 @@
+require "yaml"
+
+require "./base_config"
+
 # Convenience alias to make referencing `Athena::Config` types easier.
 alias ACF = Athena::Config
 
@@ -10,11 +14,20 @@ module Athena::Config
   # The default path to the configuration file.
   DEFAULT_CONFIG_PATH = "./athena.yml"
 
+  class_getter config : ACF::Config { ACF.load }
+
   def self.config_path : String
     ENV[CONFIG_PATH_NAME]? || DEFAULT_CONFIG_PATH
   end
 
   def self.config_path=(path : String) : Nil
     ENV[CONFIG_PATH_NAME] = path
+  end
+
+  private def def self.load : ACF::Config
+    # TODO: Handle ENV vars and params
+    ACF::Config.from_yaml {{read_file ENV[CONFIG_PATH_NAME]? || DEFAULT_CONFIG_PATH}}
+  rescue ex : YAML::ParseException
+    raise "Error parsing Athena configuration file(#{ACF.config_path}): '#{ex.message}'"
   end
 end
