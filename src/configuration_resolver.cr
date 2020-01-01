f32799480490927a0698167e9f1c1bc4863510cf
Blacksmoke16 <yomoejoe@gmail.com>
2020-01-01 14:01:40 -0500

    Add interface

--- src/configuration_resolver.cr
+++ src/configuration_resolver.cr
@@ -3,14 +3,24 @@ annotation Athena::DI::Register; end
 
 module Athena::DI::Service; end
 
-@[Athena::DI::Register]
 # Service that allows resolving a specific configuration object by type.
 #
 # Component configurations that should be resolvable must reopen `self`
 # and add a `#resolve` method with a type restriction thats returns the desired configuration.
+module Athena::Config::ConfigurationResolverInterface
+  # Returns the `ACF::Base` configuration object.
+  abstract def resolve : ACF::Base
+
+  # Resolves the configuration object for the given *_type*.
+  abstract def resolve(_type : _) : _
+end
+
+@[Athena::DI::Register]
 struct Athena::Config::ConfigurationResolver
   include Athena::DI::Service
+  include Athena::Config::ConfigurationResolverInterface
 
+  # :inherit:
   def resolve : ACF::Base
     base
   end
